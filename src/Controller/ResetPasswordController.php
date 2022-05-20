a8751ba1bb8854fb5976a0e720c9e354bd6d4304
Roun1974 <diaroun74@outlook.com>
2022-05-20 22:02:44 +0200

    Mot de passe oublie suite

--- src/Controller/ResetPasswordController.php
+++ src/Controller/ResetPasswordController.php
@@ -160,9 +160,9 @@ class ResetPasswordController extends AbstractController
         }
 
         $email = (new TemplatedEmail())
-            ->from(new Address('diaroun74@yahoo.fr', 'Arona'))
+            ->from(new Address('diaroun74@yahoo.fr', 'Asolde'))
             ->to($user->getEmail())
-            ->subject('Your password reset request')
+            ->subject('Votre demande de réinitialisation de mot de passe')
             ->htmlTemplate('reset_password/email.html.twig')
             ->context([
                 'resetToken' => $resetToken,
